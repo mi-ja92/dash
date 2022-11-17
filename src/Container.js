52f67b076d2ad90d8cf910a9176273a8affedde3
Michalina J <smolczewska.m@gmail.com>
2022-11-17 14:19:40 +0100

    2

--- src/Container.js
+++ src/Container.js
@@ -1,10 +1,9 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
-import database from "./database";
+import database from "./database.js";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
   {
     field: "name",
     headerName: "Name",
@@ -33,13 +32,11 @@ const columns = [
   },
 ];
 
-const rows = { database };
-
 export default function Container() {
   return (
     <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={database}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}

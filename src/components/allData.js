5e032b685d6d8640307ddd7732c710cc8791cb64
Michalina J <smolczewska.m@gmail.com>
2022-12-02 13:26:13 +0100

    new

--- src/components/allData.js
+++ src/components/allData.js
@@ -5,12 +5,29 @@ import { columns } from "./data/gridcolumns";
 import Box from "@mui/material/Box";
 
 export default function Container() {
+  // const allNames= data.map (student => student.name).map((student, index) => ({
+  //   ...student,
+  //   id: index + 1,}))
+
   const rows = data
     .filter((user) => user.assignment)
     .map((assignment, index) => ({
       ...assignment,
       id: index + 1,
     }));
+
+  // const allStudents = data
+  //   .filter((student) => student.name)
+  //   .map((student, index) => ({
+  //     ...student,
+  //     id: index + 1,
+  //   }));
+
+  // // let uniq = {};
+  // // const rows = allStudents.filter(
+  // //   (student) => !uniq[student.name] && (uniq[student.name] = true)
+  // // );
+
   return (
     <Box sx={{ height: 800, width: "100%" }}>
       <DataGrid
--- src/components/charts.js
+++ src/components/charts.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory";
+import { data } from "./data/database";
 
 function MyCharts() {
   return (
     <div>
-      <h1>Here come the charts</h1>
+      <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+        <VictoryBar size={5} data={data} x="assignment" y="fun" />
+      </VictoryChart>
     </div>
   );
 }
